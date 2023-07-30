09a4b51437596c18ba9e9a6529e8b5a948559ddd
Oleksii89 <108924918+Oleksii89@users.noreply.github.com>
2023-07-30 13:51:05 +0300

    update

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,37 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+const formEl = document.querySelector('.form');
+formEl.addEventListener('submit', onSubmit);
+let inputedData = '';
+const { delay, step, amount } = formEl.elements;
+
+function onSubmit(evt) {
+  evt.preventDefault();
+  inputedData = {
+    Delay: Number(delay.value),
+    Step: Number(step.value),
+    Amount: Number(amount.value),
+  };
+  for (let i = 1; i <= inputedData.Amount; i += 1) {
+    let promiseDelay = inputedData.Delay + inputedData.Step * i;
+
+    createPromise(i, promiseDelay)
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
   }
+}
+
+function createPromise(position, delay) {
+  return new Promise((res, rej) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        res({ position, delay }); // Fulfill
+      } else {
+        rej({ position, delay }); // Reject
+      }
+    }, delay);
+  });
 }
